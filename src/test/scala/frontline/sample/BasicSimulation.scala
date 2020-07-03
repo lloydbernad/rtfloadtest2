ac059e01bdb8c21319ef9075f3bd71bba23287d6
lloydbernad <31335624+lloydbernad@users.noreply.github.com>
2020-07-03 19:06:05 +0530

    Add files via upload

---
+++ src/test/scala/frontline/sample/BasicSimulation.scala
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2011-2018 GatlingCorp (https://gatling.io)
+ *
+ * All rights reserved.
+ */
+
+package frontline.sample
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import scala.concurrent.duration._
+
+class BasicSimulation extends Simulation {
+
+  val httpConf = http
+    .baseUrl("https://budweiserapi.rewardthefan.com")
+
+  val scn = scenario("scenario1")
+    .exec(
+      http("Page 0")
+        .get("/budweiserapi/TestClientSaaSConfig.json?cuId=lloyd&clId=AMIT202020")
+    )
+    .exec(
+      http("Page 1")
+        .get("/budweiserapi/TestClientSaaSConfig.json?cuId=tamil&clId=AMIT202020")
+    )
+
+  setUp(
+    scn.inject(rampUsers(100) during (10 seconds))
+  ).protocols(httpConf)
+}
