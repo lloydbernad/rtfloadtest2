c2aa64e41a57b91420fdfe6d4fdec42f1bb7d761
lloydbernad <31335624+lloydbernad@users.noreply.github.com>
2020-07-05 19:59:13 +0530

    Update BasicSimulation.scala

--- src/test/scala/frontline/sample/BasicSimulation.scala
+++ src/test/scala/frontline/sample/BasicSimulation.scala
@@ -12,21 +12,21 @@ import scala.concurrent.duration._
 
 class BasicSimulation extends Simulation {
 
-  val httpProtocol = http.baseUrl("http://google.com")
+  val httpProtocol = http.baseUrl("http://budweiserapi.rewardthefan.com/")
 
 val scn = scenario("My Scenario")
   .exec(
     http("My Request")
-      .get("/")
+      .get("budweiserapi/TestClientSaaSConfig.json?cuId=lloyd&clId=AMIT202020")
   ) 
   .exec(
     http("My Other Request")
-      .get("http://google.com")
+      .get("budweiserapi/TestClientSaaSConfig.json?cuId=tamil&clId=AMIT202020")
   ) 
 
   
 // setUp(scn.inject(rampUsers(10) during (900 seconds))).protocols(httpConf)
- setUp(scn.inject(atOnceUsers(100)).protocols(httpProtocol))
+ setUp(scn.inject(atOnceUsers(10000)).protocols(httpProtocol))
   
 // setUp(scn.inject(rampUsers(100) during (100 seconds))).protocols(httpConf)
 
