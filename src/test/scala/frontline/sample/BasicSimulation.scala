013a40fb13a9e17d8567a0d034a14c3b22398da2
lloydbernad <31335624+lloydbernad@users.noreply.github.com>
2020-07-10 02:44:44 +0530

    Update BasicSimulation.scala

--- src/test/scala/frontline/sample/BasicSimulation.scala
+++ src/test/scala/frontline/sample/BasicSimulation.scala
@@ -12,12 +12,12 @@ import scala.concurrent.duration._
 
 class BasicSimulation extends Simulation {
 
-  val httpProtocol = http.baseUrl("https://budweiserapi.rewardthefan.com/")
+  val httpProtocol = http.baseUrl("http://budweiserapi.rewardthefan.com/")
 
 val scn = scenario("My Scenario")
   .exec(
     http("HTTP Request")
-      .get("budweiserapi/TestClientSaaSConfig.json?cuId=00&clId=DEM2020BUDWEISER0620")
+      .get("budweiserapi/GetClientSaaSConfig.json?cuId=00&clId=DEM2020BUDWEISER0620")
   ) 
 
  setUp(scn.inject(atOnceUsers(400000)).protocols(httpProtocol))
