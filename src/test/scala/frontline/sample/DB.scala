cafcd1cae96771e38f77bd0f8353f3ff3ae7c023
lloydbernad <31335624+lloydbernad@users.noreply.github.com>
2020-07-14 20:01:13 +0530

    Update DB.scala

--- src/test/scala/frontline/sample/DB.scala
+++ src/test/scala/frontline/sample/DB.scala
@@ -20,5 +20,5 @@ val scn = scenario("DB")
       .get("budweiserapi/GetClientSaaSConfig.json?cuId=00&clId=DEM2020BUDWEISER0620")
   ) 
 
- setUp(scn.inject(atOnceUsers(100000)).protocols(httpProtocol))
+ setUp(scn.inject(atOnceUsers(110000)).protocols(httpProtocol))
 }
