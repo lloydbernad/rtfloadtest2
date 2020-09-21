58207149b2b84b6c06f7f5beb9fa9b60348b684c
lloydbernad <31335624+lloydbernad@users.noreply.github.com>
2020-09-22 01:17:21 +0530

    Create WriteCustomerPSTMT.scala

---
+++ src/test/scala/frontline/sample/WriteCustomerPSTMT.scala
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2011-2018 GatlingCorp (https://gatling.io)
+ *
+ * All rights reserved.
+ */
+
+package frontline.sample
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import scala.concurrent.duration._
+
+class WriteCustomerPSTMT extends Simulation {
+
+  val httpProtocol = http.baseUrl("https://clapi.rewardthefan.com/")
+
+val scn = scenario("DB INSERT-READ")
+  .exec(
+    http("SAASCONFIG-API")
+      .get("budweiserapi/TempTrackingWritePstatmnt.json?cuId=00&clId=DEM2020BUDWEISER0629")
+  ) 
+
+ setUp(scn.inject(atOnceUsers(20000)).protocols(httpProtocol))
+}
