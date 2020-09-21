186221c1d9d68824c1d23bdc53c65659fc5a17c5
lloydbernad <31335624+lloydbernad@users.noreply.github.com>
2020-09-21 22:17:47 +0530

    Create ReadCustomerNormal.scala

---
+++ src/test/scala/frontline/sample/ReadCustomerNormal.scala
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2011-2018 GatlingCorp (https://gatling.io)
+ *
+ * All rights reserved.
+ */
+
+package frontline.sample
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import scala.concurrent.duration._
+
+class ReadCustomerNormal extends Simulation {
+
+  val httpProtocol = http.baseUrl("https://clapi.rewardthefan.com/")
+
+val scn = scenario("DB INSERT-READ")
+  .exec(
+    http("SAASCONFIG-API")
+      .get("budweiserapi/TempClientSaaSConfig.json?cuId=00&clId=DEM2020BUDWEISER0620")
+  ) 
+
+ setUp(scn.inject(atOnceUsers(10000)).protocols(httpProtocol))
+}
