f2d434ab9b746d68b7a4910c6bb644e96d7d52fd
lloydbernad <31335624+lloydbernad@users.noreply.github.com>
2020-10-30 01:43:54 +0530

    Update TestjoinRCPK.scala

--- src/test/scala/frontline/sample/TestjoinRCPK.scala
+++ src/test/scala/frontline/sample/TestjoinRCPK.scala
@@ -17,8 +17,8 @@ class TestjoinRCPK extends Simulation {
 val scn = scenario("JOIN CONTEST RCPK")
   .exec(
     http("JOIN CONTEST RCPK")
-      .get("budweiserapi/livtJoinContestRCPK.json?cuId=00&clId=DEM2020BUDWEISER0629&coId=DEM2020BUDWEISER06291603914063259")
+      .get("budweiserapi/livtJoinContestRCPK.json?dbctl=0&cuId=00&clId=DEM2020BUDWEISER0629&coId=DEM2020BUDWEISER06291603914063259")
   ) 
 
- setUp(scn.inject(atOnceUsers(2000000)).protocols(httpProtocol))
+ setUp(scn.inject(atOnceUsers(100000)).protocols(httpProtocol))
 }
