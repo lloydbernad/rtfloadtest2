cc05feb8b92faefc9d2e5fe0de198538e93ff319
lloydbernad <31335624+lloydbernad@users.noreply.github.com>
2020-10-30 00:36:16 +0530

    Create TestjoinRCPK.scala

---
+++ src/test/scala/frontline/sample/TestjoinRCPK.scala
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2011-2018 GatlingCorp (https://gatling.io)
+ *
+ * All rights reserved.
+ */
+
+package frontline.sample
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import scala.concurrent.duration._
+
+class BasicSimulation extends Simulation {
+
+  val httpProtocol = http.baseUrl("https://clapi.rewardthefan.com/")
+
+val scn = scenario("DB INSERT-READ")
+  .exec(
+    http("SAASCONFIG-API")
+      .get("budweiserapi/TempClientSaaSConfig.json?cuId=00&clId=DEM2020BUDWEISER0629")
+  ) 
+
+ setUp(scn.inject(atOnceUsers(1500000)).protocols(httpProtocol))
+}
