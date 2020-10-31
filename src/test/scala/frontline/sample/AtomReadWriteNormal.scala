8bc9a471febd238c7854018cc4bc3a4a78a472e4
lloydbernad <31335624+lloydbernad@users.noreply.github.com>
2020-11-01 02:44:33 +0530

    Update AtomReadWriteNormal.scala

--- src/test/scala/frontline/sample/AtomReadWriteNormal.scala
+++ src/test/scala/frontline/sample/AtomReadWriteNormal.scala
@@ -13,11 +13,11 @@ import scala.concurrent.duration._
 class AtomReadWriteNormal extends Simulation {
 
   val httpProtocol = http.baseUrl("https://clapi.rewardthefan.com/")
-
+//livtJoinContestrcpk1rdwr2.json
 val scn = scenario("ATOMIC READ WRITE")
   .exec(
     http("ATOMIC READ WRITE")
-      .get("budweiserapi/livtJoinContestRCPK.json?cuId=00&clId=DEM2020BUDWEISER0629&coId=DEM2020BUDWEISER06291603914063259")
+      .get("budweiserapi/livtJoinContestrcpk1rdwr2.json?cuId=00&clId=DEM2020BUDWEISER0629&coId=DEM2020BUDWEISER06291603914063259")
   ) 
 
  setUp(scn.inject(atOnceUsers(1000000)).protocols(httpProtocol))
