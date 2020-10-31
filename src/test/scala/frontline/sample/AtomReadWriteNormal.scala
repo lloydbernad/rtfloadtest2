f35c999016271e3b3867ca89edba5d747cda8558
lloydbernad <31335624+lloydbernad@users.noreply.github.com>
2020-10-31 05:51:26 +0530

    Update AtomReadWriteNormal.scala

--- src/test/scala/frontline/sample/AtomReadWriteNormal.scala
+++ src/test/scala/frontline/sample/AtomReadWriteNormal.scala
@@ -20,5 +20,5 @@ val scn = scenario("ATOMIC READ WRITE")
       .get("budweiserapi/livtJoinContesttesttrackatomRW.json?cuId=00&clId=DEM2020BUDWEISER0629&coId=DEM2020BUDWEISER06291603914063259")
   ) 
 
- setUp(scn.inject(atOnceUsers(1000)).protocols(httpProtocol))
+ setUp(scn.inject(atOnceUsers(2000000)).protocols(httpProtocol))
 }
